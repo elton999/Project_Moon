13fd9b7469cdd483c8f78a854530d4cf3b4400e1
Elton Silva <eltoncarsilva73@gmail.com>
2022-03-18 23:12:56 -0300

    weapon refatoring

--- GameProject/Gameplay/Weapon.cs
+++ GameProject/Gameplay/Weapon.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Linq;
 using UmbrellaToolsKit.Collision;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using GameProject.Entities;
 using GameProject.Entities.Actors;
 
 namespace GameProject.Gameplay
@@ -14,34 +12,38 @@ namespace GameProject.Gameplay
 
         public Weapon(Actor actor)
         {
-            this._actor = actor;
+            _actor = actor;
         }
 
         private float BulletVelocity = -0.2f;
-        public bool IsFire = false;
-        private int _swingBullet = 2;
+        private int _swingBullet = 1;
 
         // Cadence
         private float Cadence = 0.5f;
         private float timerPressed = 0;
         private bool firstBullet = false;
 
+        private Vector2 _weaponPositionNormal = new Vector2(20, 5);
+        private Vector2 _weaponPositionUp = new Vector2(10, -10);
+
+        public bool IsFire = false;
+
         public void Update(GameTime gameTime, bool fire)
         {
             if (fire)
             {
-                this.IsFire = true;
+                IsFire = true;
                 timerPressed += (float)gameTime.ElapsedGameTime.TotalSeconds;
                 if (timerPressed >= Cadence || !firstBullet)
                 {
                     firstBullet = true;
                     timerPressed = 0;
-                    this.Shoot();
+                    Shoot();
                 }
             }
             else
             {
-                this.IsFire = false;
+                IsFire = false;
                 timerPressed = 0;
                 firstBullet = false;
             }
@@ -50,39 +52,29 @@ namespace GameProject.Gameplay
         public bool UpShoot = false;
         public void Shoot()
         {
-            var bullet = new Bullet();
-            bullet.Scene = this._actor.Scene;
-            bullet.spriteEffect = this._actor.spriteEffect;
+            Bullet bullet = CreateBullet();
+            Vector2 randomPosition = Vector2.UnitY * (new Random()).Next(-_swingBullet, _swingBullet);
 
-            float velocityX = this._actor.spriteEffect == SpriteEffects.None ? -this.BulletVelocity : this.BulletVelocity;
-            float velocityY = this.UpShoot ? this.BulletVelocity : 0;
-            bullet.velocity = new Vector2(velocityX, velocityY);
+                if (!UpShoot)
+                    randomPosition += _actor.spriteEffect == SpriteEffects.None ? _weaponPositionNormal : _weaponPositionNormal * new Vector2(-1, 1);
+                else
+                    randomPosition += _actor.spriteEffect == SpriteEffects.None ?  _weaponPositionUp : _weaponPositionUp * new Vector2(-1, 1);
 
-            if (this._actor.spriteEffect == SpriteEffects.None)
-            {
-                if (!this.UpShoot)
-                    bullet.Position = new Vector2(
-                        this._actor.Position.X + 20,
-                        this._actor.Position.Y + 10 + (new Random()).Next(-this._swingBullet, this._swingBullet)
-                    );
-                else
-                    bullet.Position = new Vector2(
-                        this._actor.Position.X + 10,
-                        this._actor.Position.Y - 10 + (new Random()).Next(-this._swingBullet, this._swingBullet)
-                    );
-                this._actor.moveX(-10);
-                //SmashEfx(true);
-            }
-            else
-            {
-                if (!this.UpShoot)
-                    bullet.Position = new Vector2(this._actor.Position.X - 20, this._actor.Position.Y + 10 + (new Random()).Next(-this._swingBullet, this._swingBullet));
-                else
-                    bullet.Position = new Vector2(this._actor.Position.X - 10, this._actor.Position.Y - 10 + (new Random()).Next(-this._swingBullet, this._swingBullet));
-                this._actor.moveX(10);
-                //SmashEfx(true);
-            }
+            bullet.Position = _actor.Position + randomPosition;
+
             bullet.Start();
+        }
+
+        private Bullet CreateBullet()
+        {
+            var bullet = new Bullet();
+            bullet.Scene = _actor.Scene;
+            bullet.spriteEffect = _actor.spriteEffect;
+
+            float velocityX = _actor.spriteEffect == SpriteEffects.None ? -BulletVelocity : BulletVelocity;
+            float velocityY = UpShoot ? BulletVelocity : 0;
+            bullet.velocity = new Vector2(velocityX, velocityY);
+            return bullet;
         }
     }
 }
